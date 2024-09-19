98ee33cc0c47c7e4c232246013f490a02d28ccb4
wangjiaming0909 <604227650@qq.com>
2024-09-19 16:55:04 +0800

    validation for exection nodes

--- source/libs/executor/src/timewindowoperator.c
+++ source/libs/executor/src/timewindowoperator.c
@@ -1635,7 +1635,7 @@ _end:
 }
 
 // todo make this as an non-blocking operator
-int32_t createStatewindowOperatorInfo(SOperatorInfo* downstream, SStateWinodwPhysiNode* pStateNode,
+int32_t createStatewindowOperatorInfo(SOperatorInfo* downstream, SStateWindowPhysiNode* pStateNode,
                                       SExecTaskInfo* pTaskInfo, SOperatorInfo** pOptrInfo) {
   QRY_PARAM_CHECK(pOptrInfo);
 
@@ -1746,7 +1746,7 @@ void destroySWindowOperatorInfo(void* param) {
   taosMemoryFreeClear(param);
 }
 
-int32_t createSessionAggOperatorInfo(SOperatorInfo* downstream, SSessionWinodwPhysiNode* pSessionNode,
+int32_t createSessionAggOperatorInfo(SOperatorInfo* downstream, SSessionWindowPhysiNode* pSessionNode,
                                      SExecTaskInfo* pTaskInfo, SOperatorInfo** pOptrInfo) {
   QRY_PARAM_CHECK(pOptrInfo);
 
